daafac71908bf9bae8df8bb702d0e339e2cd08f2
Aditya Hajare <aditya43@gmail.com>
2020-10-07 17:48:06 +0530

    Enabled CORS

--- 01-Basic-Microservices-APP/posts/index.js
+++ 01-Basic-Microservices-APP/posts/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const posts = {};
 
