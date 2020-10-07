00c1812e8b3086a05baccc52d8280649d4934386
Aditya Hajare <aditya43@gmail.com>
2020-10-07 18:06:07 +0530

    Client React App

--- 01-Basic-Microservices-APP/client/src/CommentCreate.js
+++ 01-Basic-Microservices-APP/client/src/CommentCreate.js
@@ -1,27 +1,28 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-export default () => {
-  const [title, setTitle] = useState('');
+export default ({ postId }) => {
+  const [content, setContent] = useState('');
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    await axios.post('http://localhost:4000/posts', {
-      title,
+
+    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+      content,
     });
 
-    setTitle('');
+    setContent('');
   };
 
   return (
     <div>
       <form onSubmit={onSubmit}>
         <div className="form-group">
-          <label>Title</label>
+          <label>New Comment</label>
           <input
             className="form-control"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
           />
         </div>
         <button className="btn btn-primary">Submit</button>
