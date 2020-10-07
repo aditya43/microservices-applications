2b4fec58a10e057ebf6059272d1e3a1895d30893
Aditya Hajare <aditya43@gmail.com>
2020-10-07 18:10:36 +0530

    Client React App

--- 01-Basic-Microservices-APP/client/src/CommentCreate.js
+++ 01-Basic-Microservices-APP/client/src/CommentCreate.js
@@ -2,31 +2,31 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 export default ({ postId }) => {
-  const [content, setContent] = useState('');
+    const [content, setContent] = useState('');
 
-  const onSubmit = async (event) => {
-    event.preventDefault();
+    const onSubmit = async (event) => {
+        event.preventDefault();
 
-    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
-      content,
-    });
+        await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+            content,
+        });
 
-    setContent('');
-  };
+        setContent('');
+    };
 
-  return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <div className="form-group">
-          <label>New Comment</label>
-          <input
-            className="form-control"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label>New Comment</label>
+                    <input
+                        className="form-control"
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                    />
+                </div>
+                <button className="btn btn-primary">Submit</button>
+            </form>
         </div>
-        <button className="btn btn-primary">Submit</button>
-      </form>
-    </div>
-  );
+    );
 };
