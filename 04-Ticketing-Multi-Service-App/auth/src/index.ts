66ad68fd69c7516d449a385e8fd49b95bab53c18
Aditya Hajare <aditya43@gmail.com>
2020-10-11 08:39:47 +0530

    Added currentuser route

--- 04-Ticketing-Multi-Service-App/auth/src/index.ts
+++ 04-Ticketing-Multi-Service-App/auth/src/index.ts
@@ -4,6 +4,11 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
+app.get('/api/users/currentuser', (_, res) => {
+    console.log('Aditya Test 123');
+    res.send('Hello Aditya');
+});
+
 app.listen(3000, () => {
     console.log('Auth service listening on 3000');
 });
