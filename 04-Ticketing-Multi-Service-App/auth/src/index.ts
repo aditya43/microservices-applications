3cb936182a59a4a5ace16f70452f87bd9cd890ad
Aditya Hajare <aditya43@gmail.com>
2020-10-11 12:00:32 +0530

    Error Handler

--- 04-Ticketing-Multi-Service-App/auth/src/index.ts
+++ 04-Ticketing-Multi-Service-App/auth/src/index.ts
@@ -5,6 +5,7 @@ import { currentUserRouter } from './routes/curent-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
 app.use(json());
@@ -13,6 +14,8 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
     console.log('Auth service listening on 3000');
---
+++ 04-Ticketing-Multi-Service-App/auth/src/middlewares/error-handler.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from 'express';
+
+export const errorHandler = (
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    console.log('Something went wrong! ', err);
+
+    res.status(400).send({ message: err.message });
+
+    req;
+    next;
+};
