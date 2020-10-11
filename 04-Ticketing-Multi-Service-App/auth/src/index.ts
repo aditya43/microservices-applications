464a33a67563c96f126149fa7b9905673863fd16
Aditya Hajare <aditya43@gmail.com>
2020-10-11 09:18:30 +0530

    Added routes

--- 04-Ticketing-Multi-Service-App/auth/src/index.ts
+++ 04-Ticketing-Multi-Service-App/auth/src/index.ts
@@ -2,10 +2,17 @@ import express from 'express';
 import { json } from 'body-parser';
 
 import { currentUserRouter } from './routes/curent-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
 
 const app = express();
 app.use(json());
+
 app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.listen(3000, () => {
     console.log('Auth service listening on 3000');
