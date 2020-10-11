d71047046ca09abea7f4b2a32e14bb31c38a83ec
Aditya Hajare <aditya43@gmail.com>
2020-10-11 09:09:09 +0530

    Refactored currentuser router

--- 04-Ticketing-Multi-Service-App/auth/src/index.ts
+++ 04-Ticketing-Multi-Service-App/auth/src/index.ts
@@ -1,13 +1,11 @@
 import express from 'express';
 import { json } from 'body-parser';
 
+import { currentUserRouter } from './routes/curent-user';
+
 const app = express();
 app.use(json());
-
-app.get('/api/users/currentuser', (_, res) => {
-    console.log('Aditya Test 123');
-    res.send('Hello Aditya');
-});
+app.use(currentUserRouter);
 
 app.listen(3000, () => {
     console.log('Auth service listening on 3000');
