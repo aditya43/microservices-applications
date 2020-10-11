93cd18960d21357eabae09582aae0e56fda4f827
Aditya Hajare <aditya43@gmail.com>
2020-10-11 10:56:43 +0530

    Signup

--- 04-Ticketing-Multi-Service-App/auth/src/routes/signup.ts
+++ 04-Ticketing-Multi-Service-App/auth/src/routes/signup.ts
@@ -1,9 +1,29 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (_, res) => {
-    res.send('Hello Aditya | Signup Route');
-});
+router.post(
+    '/api/users/signup',
+    [
+        body('email').isEmail().withMessage('Invalid email address'),
+        body('password')
+            .trim()
+            .isLength({ min: 4, max: 20 })
+            .withMessage('Password must be between 4 and 20 characters'),
+    ],
+    (req: Request, res: Response): Response => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).send(errors.array());
+        }
+
+        const { email, password } = req.body;
+
+        console.log(`Creating User ${email} | ${password}`);
+        return res.send({ email, password });
+    },
+);
 
 export { router as signupRouter };
