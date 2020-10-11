742b8214db03a949adc01631d2f88b4cbebd122f
Aditya Hajare <aditya43@gmail.com>
2020-10-11 12:18:13 +0530

    Error handlers refactored

--- 04-Ticketing-Multi-Service-App/auth/src/errors/database-connection-error.ts
+++
@@ -1,9 +0,0 @@
-import { ValidationError } from 'express-validator';
-
-export class DatabaseConnectionError extends Error {
-    statusCode = 400;
-
-    constructor(private errors: ValidationError[]) {
-        super();
-    }
-}
--- 04-Ticketing-Multi-Service-App/auth/src/errors/request-validation.ts
+++ 04-Ticketing-Multi-Service-App/auth/src/errors/request-validation.ts
@@ -1,5 +1,5 @@
 import { ValidationError } from 'express-validator';
-import { CustomError } from './custom-error';
+import { CustomError } from './custom';
 
 export class RequestValidationError extends CustomError {
     statusCode = 400;
