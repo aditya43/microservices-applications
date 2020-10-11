9821c976075b9deca6e27788d45ba108d7b55bb5
Aditya Hajare <aditya43@gmail.com>
2020-10-11 17:45:44 +0530

    Added MongoDB

--- 05-Multi-Service-MongoDB/auth/src/index.ts
+++ 05-Multi-Service-MongoDB/auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -24,6 +25,22 @@ app.all('*', async () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-    console.log('Auth service listening on port 3000');
-});
+const init = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-svc:27017/auth', {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex: true,
+        });
+
+        console.info('Connected to Auth MongoDB');
+    } catch (error) {
+        console.error(error);
+    }
+
+    app.listen(3000, () => {
+        console.log('Auth service listening on port 3000');
+    });
+};
+
+init();
