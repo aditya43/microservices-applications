d26b41a54b3e4607dc442646502eeaabf3afbe93
Aditya Hajare <aditya43@gmail.com>
2020-10-12 12:15:14 +0530

    Updated User Schema

--- 05-Multi-Service-App-MongoDB/auth/src/models/user.ts
+++ 05-Multi-Service-App-MongoDB/auth/src/models/user.ts
@@ -9,23 +9,40 @@ interface UserPropsInterface {
 
 // An interface that describes the properties
 // that a User model has.
-interface UserModelInterface extends mongoose.Model<any> {
-    build(params: UserPropsInterface): any;
+interface UserModelInterface extends mongoose.Model<UserDocInterface> {
+    build(params: UserPropsInterface): UserDocInterface;
 }
 
-const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
+// An interface that describes the properties
+// that a User Document has.
+interface UserDocInterface extends mongoose.Document {
+    email: string;
+    password: string;
+    createdAt?: string;
+    updatedAt?: string;
+}
+
+const userSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
     },
-    password: {
-        type: String,
-        required: true,
+    {
+        timestamps: true,
     },
-});
+);
 
 userSchema.statics.build = (params: UserPropsInterface) => new User(params);
 
-const User = mongoose.model<any, UserModelInterface>('User', userSchema);
+const User = mongoose.model<UserDocInterface, UserModelInterface>(
+    'User',
+    userSchema,
+);
 
 export { User };
