bb2707c6ba68eba0c2f83641d0866b4746ba7ecf
Aditya Hajare <aditya43@gmail.com>
2020-10-13 21:42:47 +0530

    Current user route

--- 06-Multi-Service-App-Authentication/auth/src/routes/current-user.ts
+++ 06-Multi-Service-App-Authentication/auth/src/routes/current-user.ts
@@ -1,10 +1,22 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (_, res) => {
-    console.log('Aditya Test 123');
-    res.send('Hello Aditya');
-});
+router.get(
+    '/api/users/currentuser',
+    (req: Request, res: Response): Response | void => {
+        if (!req.session?.jwt) {
+            return res.send({ currentUser: null });
+        }
+
+        try {
+            const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+            res.send({ currentUser: payload });
+        } catch (e) {
+            res.send({ currentUser: null });
+        }
+    },
+);
 
 export { router as currentUserRouter };
