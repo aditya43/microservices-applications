d7b6e01a1f3942a59315a630420527e5783a4bb9
Aditya Hajare <aditya43@gmail.com>
2020-10-13 19:26:31 +0530

    Validate request ML added to signin route

--- 06-Multi-Service-App-Authentication/auth/src/routes/signin.ts
+++ 06-Multi-Service-App-Authentication/auth/src/routes/signin.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
-import { RequestValidationError } from '../errors/request-validation';
+import { body } from 'express-validator';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
@@ -13,13 +13,8 @@ router.post(
             .notEmpty()
             .withMessage('Password cannot be empty!'),
     ],
-    async (req: Request, res: Response) => {
-        const errors = validationResult(req);
-
-        if (!errors.isEmpty()) {
-            throw new RequestValidationError(errors.array());
-        }
-    },
+    validateRequest,
+    async (req: Request, res: Response) => {},
 );
 
 export { router as signinRouter };
