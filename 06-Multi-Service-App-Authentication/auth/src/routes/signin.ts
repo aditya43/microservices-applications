71100bbc3277e7d1caf44cf8267e687b0509abdb
Aditya Hajare <aditya43@gmail.com>
2020-10-13 19:15:19 +0530

    Sign In validations added

--- 06-Multi-Service-App-Authentication/auth/src/routes/signin.ts
+++ 06-Multi-Service-App-Authentication/auth/src/routes/signin.ts
@@ -1,9 +1,25 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (_, res) => {
-    res.send('Hello Aditya | Signin Route');
-});
+router.post(
+    '/api/users/signin',
+    [
+        body('email').isEmail().withMessage('Invalid Email Address!'),
+        body('password')
+            .trim()
+            .notEmpty()
+            .withMessage('Password cannot be empty!'),
+    ],
+    async (req: Request, res: Response) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            throw new RequestValidationError(errors.array());
+        }
+    },
+);
 
 export { router as signinRouter };
