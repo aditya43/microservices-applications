08141546104ab6a94d03068daae242e209dd4f39
Aditya Hajare <aditya43@gmail.com>
2020-10-13 19:25:02 +0530

    Request validate middleware added to signup route

--- 06-Multi-Service-App-Authentication/auth/src/routes/signup.ts
+++ 06-Multi-Service-App-Authentication/auth/src/routes/signup.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
-import { body, validationResult } from 'express-validator';
+
+import { body } from 'express-validator';
 import { User } from '../models/user';
-import { RequestValidationError } from '../errors/request-validation';
+import { validateRequest } from '../middlewares/validate-request';
 import { BadRequestError } from '../errors/bad-request';
 
 const router = express.Router();
@@ -16,13 +17,8 @@ router.post(
             .isLength({ min: 4, max: 20 })
             .withMessage('Password must be between 4 and 20 characters'),
     ],
+    validateRequest,
     async (req: Request, res: Response) => {
-        const errors = validationResult(req);
-
-        if (!errors.isEmpty()) {
-            throw new RequestValidationError(errors.array());
-        }
-
         const { email, password } = req.body;
         const existingUser = await User.findOne({ email });
 
