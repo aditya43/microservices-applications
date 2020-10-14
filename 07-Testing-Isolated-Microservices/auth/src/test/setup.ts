2cc9612eb687a1b3358ded4af40687999278670f
Aditya Hajare <aditya43@gmail.com>
2020-10-14 11:41:17 +0530

    Jest beforeAll() setup

--- 07-Testing-Isolated-Microservices/auth/src/test/setup.ts
+++ 07-Testing-Isolated-Microservices/auth/src/test/setup.ts
@@ -1,3 +1,13 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { app } from '../app';
+
+beforeAll(async () => {
+    const mongo = new MongoMemoryServer();
+    const mongoUri = await mongo.getUri();
+
+    await mongoose.connect(mongoUri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+});
