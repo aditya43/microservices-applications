707f4acc5b5690543e3e1fc6496dbb84010eb9e3
Aditya Hajare <aditya43@gmail.com>
2020-10-14 19:14:41 +0530

    Current user tests

--- 07-Testing-Isolated-Microservices/auth/src/test/setup.ts
+++ 07-Testing-Isolated-Microservices/auth/src/test/setup.ts
@@ -1,6 +1,15 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-// import { app } from '../app';
+import request from 'supertest';
+import { app } from '../app';
+
+declare global {
+    namespace NodeJS {
+        interface Global {
+            signin(): Promise<string[]>;
+        }
+    }
+}
 
 let mongo: MongoMemoryServer;
 beforeAll(async () => {
@@ -26,3 +35,19 @@ afterAll(async () => {
     await mongo.stop();
     await mongoose.connection.close();
 });
+
+global.signin = async () => {
+    const email = 'test@test.com';
+    const password = 'password';
+
+    const response = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email,
+            password,
+        })
+        .expect(201);
+
+    const cookie = response.get('Set-Cookie');
+    return cookie;
+};
