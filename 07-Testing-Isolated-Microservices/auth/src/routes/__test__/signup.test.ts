267ba4619ab0c0ffc83e7907447a66d9a1778597
Aditya Hajare <aditya43@gmail.com>
2020-10-14 14:13:46 +0530

    Signup Tests

--- 07-Testing-Isolated-Microservices/auth/src/routes/__test__/signup.test.ts
+++ 07-Testing-Isolated-Microservices/auth/src/routes/__test__/signup.test.ts
@@ -64,3 +64,15 @@ it('disallows duplicate emails', async () => {
         })
         .expect(400);
 });
+
+it('sets a cookie after successful signup', async () => {
+    const response = await request(app) // No need to return if using await.
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'aditya12345',
+        })
+        .expect(201);
+
+    expect(response.get('Set-Cookie')).toBeDefined();
+});
