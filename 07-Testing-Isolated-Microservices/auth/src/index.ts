cf5b1bef58522ff55b4fd0d0c3df3fd9cc37fa6f
Aditya Hajare <aditya43@gmail.com>
2020-10-14 11:25:21 +0530

    Refactored index file to split express app

--- 07-Testing-Isolated-Microservices/auth/src/index.ts
+++ 07-Testing-Isolated-Microservices/auth/src/index.ts
@@ -1,37 +1,5 @@
-import express from 'express';
-import 'express-async-errors';
-
-import { json } from 'body-parser';
+import { app } from './app';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found';
-
-const app = express();
-app.set('trust proxy', true); // To make express know that it is behind the proxy of ingress and trust the traffic
-app.use(json());
-app.use(
-    cookieSession({
-        signed: false,
-        secure: true,
-    }),
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', async () => {
-    throw new NotFoundError();
-});
-
-app.use(errorHandler);
 
 const init = async () => {
     if (!process.env.JWT_KEY) {
