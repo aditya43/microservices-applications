22645a57855205b7389f89512adb0042494cb9ab
Aditya Hajare <aditya43@gmail.com>
2020-10-15 15:34:14 +0530

    Fixed getaddrinfo ENOTFOUND | Kubernetes

--- 09-Code-Sharing-Between-Services/client/api/build-client.js
+++ 09-Code-Sharing-Between-Services/client/api/build-client.js
@@ -3,7 +3,13 @@ import axios from 'axios';
 export default ({ req }) => {
     if (typeof window === 'undefined') {
         return axios.create({
-            baseURL: 'http://172-17-0-2.kubernetes.default.svc.cluster.local',
+            // baseURL: 'http://ingress-nginx.ingress-nginx.svc.cluster.local',
+            // baseURL: 'http://172-17-0-2.kubernetes.default.svc.cluster.local',
+            baseURL:
+                'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+            // baseURL:
+            // 'http://ingress-nginx.ingress-nginx-controller.svc.cluster.local',
+            // baseURL: 'http://ingress-svc.default.svc.cluster.local',
             headers: req.headers,
         });
     } else {
