9084d1984dd1cd28ba2519e43e9e22348be0dc78
Aditya Hajare <aditya43@gmail.com>
2020-10-16 17:43:26 +0530

    Using Mongo URI from ENV

--- 10-CRUD-Ticketing-Service/auth/src/index.ts
+++ 10-CRUD-Ticketing-Service/auth/src/index.ts
@@ -2,12 +2,12 @@ import { app } from './app';
 import mongoose from 'mongoose';
 
 const init = async () => {
-    if (!process.env.JWT_KEY) {
-        throw new Error('JWT_KEY is not set in environment');
+    if (!process.env.JWT_KEY || !process.env.MONGO_URI) {
+        throw new Error('JWT_KEY or MONGO_URI is not set in environment');
     }
 
     try {
-        await mongoose.connect('mongodb://auth-mongo-svc:27017/auth', {
+        await mongoose.connect(process.env.MONGO_URI, {
             useUnifiedTopology: true,
             useNewUrlParser: true,
             useCreateIndex: true,
