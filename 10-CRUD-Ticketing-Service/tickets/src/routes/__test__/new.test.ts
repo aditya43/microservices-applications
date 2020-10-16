f3c084575b0b8bf7c14f02f700c9c5d9b32f323f
Aditya Hajare <aditya43@gmail.com>
2020-10-16 17:52:55 +0530

    Added boilerplates for tests

--- 10-CRUD-Ticketing-Service/tickets/src/routes/__test__/new.test.ts
+++ 10-CRUD-Ticketing-Service/tickets/src/routes/__test__/new.test.ts
@@ -2,3 +2,9 @@ import request from 'supertest';
 import { app } from '../../app';
 
 it('has a route handler listening to /api/tickets for POST requests', async () => {});
+
+it('can only be accessed if the user is signed in', async () => {});
+
+it('returns an error if invalid title is provided', async () => {});
+
+it('creates a ticket when valid inputs are provided', async () => {});
