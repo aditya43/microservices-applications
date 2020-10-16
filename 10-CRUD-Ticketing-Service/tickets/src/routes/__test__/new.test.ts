8dbbff1d6c6fdaa5b91957d17bdf8047a255bbdc
Aditya Hajare <aditya43@gmail.com>
2020-10-16 17:58:28 +0530

    Test if route is present

--- 10-CRUD-Ticketing-Service/tickets/src/routes/__test__/new.test.ts
+++ 10-CRUD-Ticketing-Service/tickets/src/routes/__test__/new.test.ts
@@ -1,7 +1,11 @@
 import request from 'supertest';
 import { app } from '../../app';
 
-it('has a route handler listening to /api/tickets for POST requests', async () => {});
+it('has a route handler listening to /api/tickets for POST requests', async () => {
+    const response = await request(app).post('/api/tickets').send({});
+
+    expect(response.status).not.toEqual(404);
+});
 
 it('can only be accessed if the user is signed in', async () => {});
 
