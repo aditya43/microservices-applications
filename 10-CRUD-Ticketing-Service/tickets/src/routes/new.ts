219586d124d46e56a03620a506a729e4c7981cab
Aditya Hajare <aditya43@gmail.com>
2020-10-17 18:48:29 +0530

    Create new Ticket route

--- 10-CRUD-Ticketing-Service/tickets/src/routes/new.ts
+++ 10-CRUD-Ticketing-Service/tickets/src/routes/new.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@adi-microservices/common';
+import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
@@ -14,8 +15,17 @@ router.post(
             .withMessage('Price must be greater than 0'),
     ],
     validateRequest,
-    (req: Request, res: Response) => {
-        res.send(JSON.stringify(req, null, 1));
+    async (req: Request, res: Response) => {
+        const { title, price } = req.body;
+
+        const ticket = Ticket.build({
+            title,
+            price,
+            userId: req.currentUser!.id,
+        });
+
+        await ticket.save();
+        res.status(201).send(ticket);
     },
 );
 
