8f58458e1f11aa00b1f987c95dd0c2db7e7a160f
Aditya Hajare <aditya43@gmail.com>
2020-10-17 09:53:25 +0530

    New ticket validations

--- 10-CRUD-Ticketing-Service/tickets/src/routes/new.ts
+++ 10-CRUD-Ticketing-Service/tickets/src/routes/new.ts
@@ -1,10 +1,22 @@
 import express, { Request, Response } from 'express';
-import { requireAuth } from '@adi-microservices/common';
+import { body } from 'express-validator';
+import { requireAuth, validateRequest } from '@adi-microservices/common';
 
 const router = express.Router();
 
-router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
-    res.send(JSON.stringify(req, null, 1));
-});
+router.post(
+    '/api/tickets',
+    requireAuth,
+    [
+        body('title').not().isEmpty().withMessage('Title is required!'),
+        body('price')
+            .isFloat({ gt: 0 })
+            .withMessage('Price must be greater than 0'),
+    ],
+    validateRequest,
+    (req: Request, res: Response) => {
+        res.send(JSON.stringify(req, null, 1));
+    },
+);
 
 export { router as createTicketRouter };
