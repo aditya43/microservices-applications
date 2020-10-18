f9730020f72dcc677eae1458666f64e2cb3282ca
Aditya Hajare <aditya43@gmail.com>
2020-10-18 12:18:44 +0530

    Create ticket

--- 10-CRUD-Ticketing-Service/tickets/src/routes/__test__/show.test.ts
+++ 10-CRUD-Ticketing-Service/tickets/src/routes/__test__/show.test.ts
@@ -5,4 +5,13 @@ it('returns 404 if the ticket is not found', async () => {
     await request(app).get('/api/tickets/fakeTicketId').send().expect(404);
 });
 
-it('returns ticket if it is found', async () => {});
+it('returns ticket if it is found', async () => {
+    const title = 'Aditya Test Ticket';
+    const price = 20;
+
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({ title, price })
+        .expect(201);
+});
