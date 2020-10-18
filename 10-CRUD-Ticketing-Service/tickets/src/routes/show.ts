01739737d9bb83c48e0f8095cb7e5b64123e4cf9
Aditya Hajare <aditya43@gmail.com>
2020-10-18 12:32:04 +0530

    Find ticket by id

--- 10-CRUD-Ticketing-Service/tickets/src/routes/show.ts
+++ 10-CRUD-Ticketing-Service/tickets/src/routes/show.ts
@@ -1,8 +1,17 @@
 import express, { Request, Response } from 'express';
 import { Ticket } from '../models/ticket';
+import { NotFoundError } from '@adi-microservices/common';
 
 const router = express.Router();
 
-router.get('/api/tickets/:id', async (req: Request, res: Response) => {});
+router.get('/api/tickets/:id', async (req: Request, res: Response) => {
+    const ticket = await Ticket.findById(req.params.id);
+
+    if (!ticket) {
+        throw new NotFoundError();
+    }
+
+    res.send(ticket);
+});
 
 export { router as showTicketRouter };
