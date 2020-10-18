5adff7c86451a762f26c501a3c977e8484d92410
Aditya Hajare <aditya43@gmail.com>
2020-10-18 12:23:09 +0530

    Show ticket test | Valid

--- 10-CRUD-Ticketing-Service/tickets/src/routes/__test__/show.test.ts
+++ 10-CRUD-Ticketing-Service/tickets/src/routes/__test__/show.test.ts
@@ -14,4 +14,12 @@ it('returns ticket if it is found', async () => {
         .set('Cookie', global.signin())
         .send({ title, price })
         .expect(201);
+
+    const ticketResponse = await request(app)
+        .get(`/api/tickets/${response.body.id}`)
+        .send()
+        .expect(200);
+
+    expect(ticketResponse.body.title).toEqual(title);
+    expect(ticketResponse.body.price).toEqual(price);
 });
