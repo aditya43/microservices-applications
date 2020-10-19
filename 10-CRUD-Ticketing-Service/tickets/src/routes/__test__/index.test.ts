8571567d93e7c497859cf7a3d48c53a585a002b3
Aditya Hajare <aditya43@gmail.com>
2020-10-19 08:15:49 +0530

    Get tickets test

--- 10-CRUD-Ticketing-Service/tickets/src/routes/__test__/index.test.ts
+++ 10-CRUD-Ticketing-Service/tickets/src/routes/__test__/index.test.ts
@@ -11,4 +11,12 @@ const createTicket = () => {
         });
 };
 
-it('can fetch a list of tickets', async () => {});
+it('can fetch a list of tickets', async () => {
+    await createTicket();
+    await createTicket();
+    await createTicket();
+
+    const response = await request(app).get('/api/tickets').send().expect(200);
+
+    expect(response.body.length).toEqual(3);
+});
