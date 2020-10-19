408e3b4de767f8c0457483982e20baffbaf4a5b8
Aditya Hajare <aditya43@gmail.com>
2020-10-19 08:22:58 +0530

    indexTicketRouter wired up

--- 10-CRUD-Ticketing-Service/tickets/src/app.ts
+++ 10-CRUD-Ticketing-Service/tickets/src/app.ts
@@ -12,6 +12,7 @@ import {
 
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
 
 const app = express();
 app.set('trust proxy', true); // To make express know that it is behind the proxy of ingress and trust the traffic
@@ -27,6 +28,7 @@ app.use(currentUser);
 
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all('*', async () => {
     throw new NotFoundError();
