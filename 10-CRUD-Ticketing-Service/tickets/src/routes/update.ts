f1371fa7e043cd36ee255230f79f490e42755153
Aditya Hajare <aditya43@gmail.com>
2020-10-19 08:45:36 +0530

    Update ticket

--- 10-CRUD-Ticketing-Service/tickets/src/routes/update.ts
+++ 10-CRUD-Ticketing-Service/tickets/src/routes/update.ts
@@ -10,6 +10,16 @@ import {
 
 const router = express.Router();
 
-router.put('/api/tickets/:id', async (req: Request, res: Response) => {});
+router.put(
+    '/api/tickets/:id',
+    requireAuth,
+    async (req: Request, res: Response) => {
+        const ticket = await Ticket.findById(req.params.id);
+
+        if (!ticket) {
+            throw new NotFoundError();
+        }
+    },
+);
 
 export { router as updateTicketRouter };
