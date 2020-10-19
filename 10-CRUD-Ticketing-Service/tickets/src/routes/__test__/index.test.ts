1b1064f2564ed99e148a8d9ed13a70a94cd5e6cd
Aditya Hajare <aditya43@gmail.com>
2020-10-19 08:13:26 +0530

    createTicket Helper

--- 10-CRUD-Ticketing-Service/tickets/src/routes/__test__/index.test.ts
+++ 10-CRUD-Ticketing-Service/tickets/src/routes/__test__/index.test.ts
@@ -1,4 +1,14 @@
 import request from 'supertest';
 import { app } from '../../app';
 
+const createTicket = () => {
+    return request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'Adi Test',
+            price: 20,
+        });
+};
+
 it('can fetch a list of tickets', async () => {});
