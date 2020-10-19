3d485fb1aee29ac2798a2b77936e7513e2789777
Aditya Hajare <aditya43@gmail.com>
2020-10-19 08:36:07 +0530

    Ticket not found test

--- 10-CRUD-Ticketing-Service/tickets/src/routes/__test__/update.test.ts
+++ 10-CRUD-Ticketing-Service/tickets/src/routes/__test__/update.test.ts
@@ -2,7 +2,18 @@ import request from 'supertest';
 import { app } from '../../app';
 import mongoose from 'mongoose';
 
-it('return a 404 if the provided id does not exist', async () => {});
+it('return a 404 if the provided id does not exist', async () => {
+    const id = mongoose.Types.ObjectId().toHexString();
+
+    await request(app)
+        .put(`/api/tickets/${id}`)
+        .set('Cookie', global.signin())
+        .send({
+            title: 'aslkdfj',
+            price: 20,
+        })
+        .expect(404);
+});
 
 it('returns a 401 if the user is not authenticated', async () => {});
 
