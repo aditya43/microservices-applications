258a89e43e20cea23392c86d02df22b4c9e1e5e8
Aditya Hajare <aditya43@gmail.com>
2020-10-19 08:37:34 +0530

    Test | updates the ticket provided valid input

--- 10-CRUD-Ticketing-Service/tickets/src/routes/__test__/update.test.ts
+++ 10-CRUD-Ticketing-Service/tickets/src/routes/__test__/update.test.ts
@@ -78,4 +78,31 @@ it('return a 400 if the user provides an invalid title or price', async () => {
         .expect(400);
 });
 
-it('updates the ticket provided valid input', async () => {});
+it('updates the ticket provided valid input', async () => {
+    const cookie = global.signin();
+
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', cookie)
+        .send({
+            title: 'aslkdfj',
+            price: 20,
+        })
+        .expect(201);
+
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set('Cookie', cookie)
+        .send({
+            title: 'new title',
+            price: 100,
+        })
+        .expect(200);
+
+    const ticketResponse = await request(app)
+        .get(`/api/tickets/${response.body.id}`)
+        .send();
+
+    expect(ticketResponse.body.title).toEqual('new title');
+    expect(ticketResponse.body.price).toEqual(100);
+});
