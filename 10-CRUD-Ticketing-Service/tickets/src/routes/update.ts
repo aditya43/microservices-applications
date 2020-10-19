a1c9d096cc5172a75cd0da7c2311953daa90303e
Aditya Hajare <aditya43@gmail.com>
2020-10-19 08:52:58 +0530

    Update ticket validations

--- 10-CRUD-Ticketing-Service/tickets/src/routes/update.ts
+++ 10-CRUD-Ticketing-Service/tickets/src/routes/update.ts
@@ -13,11 +13,22 @@ const router = express.Router();
 router.put(
     '/api/tickets/:id',
     requireAuth,
+    [
+        body('title').not().isEmpty().withMessage('Title is required'),
+        body('price')
+            .isFloat({ gt: 0 })
+            .withMessage('Price is required and must be greater than 0'),
+    ],
+    validateRequest,
     async (req: Request, res: Response) => {
         const ticket = await Ticket.findById(req.params.id);
 
         if (!ticket) {
             throw new NotFoundError();
+        }
+
+        if (ticket.userId != req.currentUser!.id) {
+            throw new NotAuthorized();
         }
     },
 );
