e341dd499b5407093df8537438cded3ff4d856f1
Aditya Hajare <aditya43@gmail.com>
2020-10-20 22:03:28 +0530

    Added Random Bytes

--- 11-NATS-Streaming-Server-Event-Bus/nats-test-project/src/publisher.ts
+++ 11-NATS-Streaming-Server-Event-Bus/nats-test-project/src/publisher.ts
@@ -1,6 +1,7 @@
 import nats from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
 
-const client = nats.connect('ticketing', 'abc', {
+const client = nats.connect('ticketing', randomBytes(4).toString('hex'), {
     url: 'http://localhost:4222',
 });
 
