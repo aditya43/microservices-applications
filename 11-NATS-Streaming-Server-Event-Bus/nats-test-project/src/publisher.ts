0dd1881eddd41a7e21b9b0adc76b8898829c0431
Aditya Hajare <aditya43@gmail.com>
2020-10-20 21:44:28 +0530

    Event Publisher

--- 11-NATS-Streaming-Server-Event-Bus/nats-test-project/src/publisher.ts
+++ 11-NATS-Streaming-Server-Event-Bus/nats-test-project/src/publisher.ts
@@ -6,4 +6,14 @@ const client = nats.connect('ticketing', 'abc', {
 
 client.on('connect', () => {
     console.log('Publisher connected to NATS');
+
+    const data = JSON.stringify({
+        id: '123',
+        title: 'Aditya Testing',
+        price: 20,
+    });
+
+    client.publish('ticket:created', data, () => {
+        console.log('Event published');
+    });
 });
