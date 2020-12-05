db212c590f8929cb5fe93bde9326c24e27036409
Aditya Hajare <aditya43@gmail.com>
2020-12-05 09:38:19 +0530

    Subjects

---
+++ 12-Connecting-NATS-In-NodeJS/nats-test-project/src/events/subjects.ts
@@ -0,0 +1,11 @@
+export enum Subjects {
+    TicketCreated = 'ticket:created',
+    OrderUpdated = 'order:updated',
+}
+
+// Example - How to use:
+// function testEnum(sub: Subjects) {
+//     console.log(sub);
+// }
+
+// testEnum(Subjects.TicketCreated);
