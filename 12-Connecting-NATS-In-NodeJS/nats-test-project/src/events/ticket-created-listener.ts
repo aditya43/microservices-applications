e3b1240986b2d7d2347d651eb05f6d25841e8f2c
Aditya Hajare <aditya43@gmail.com>
2020-12-05 10:25:31 +0530

    Refactor

--- 12-Connecting-NATS-In-NodeJS/nats-test-project/src/events/ticket-created-listener.ts
+++ 12-Connecting-NATS-In-NodeJS/nats-test-project/src/events/ticket-created-listener.ts
@@ -1,11 +1,13 @@
 import { Listener } from './abstract-listener';
 import { Message } from 'node-nats-streaming';
+import { TicketCreatedEvent } from './ticket-created-event';
+import { Subjects } from './subjects';
 
-export class TicketCreatedListener extends Listener {
-    subject = 'ticket:created';
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+    subject: Subjects.TicketCreated = Subjects.TicketCreated;
     queueGroupName = 'payments-service';
 
-    onMessage(data: any, msg: Message) {
+    onMessage(data: TicketCreatedEvent['data'], msg: Message) {
         console.log(`Event Data: ${data}`);
 
         msg.ack();
