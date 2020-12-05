13969b98ba9866a6e6da6a5fde85755ff4a37623
Aditya Hajare <aditya43@gmail.com>
2020-12-05 09:50:47 +0530

    Event interface

--- 12-Connecting-NATS-In-NodeJS/nats-test-project/src/events/abstract-listener.ts
+++ 12-Connecting-NATS-In-NodeJS/nats-test-project/src/events/abstract-listener.ts
@@ -1,9 +1,14 @@
 import { Stan, Message } from 'node-nats-streaming';
+import { Subjects } from './subjects';
 
-export abstract class Listener {
-    abstract subject: string;
+interface Event {
+    subject: Subjects;
+    data: any;
+}
+export abstract class Listener<T extends Event> {
+    abstract subject: T['subject'];
     abstract queueGroupName: string;
-    abstract onMessage(data: any, msg: Message): void;
+    abstract onMessage(data: T['data'], msg: Message): void;
 
     protected ackWait = 5 * 1000;
 
