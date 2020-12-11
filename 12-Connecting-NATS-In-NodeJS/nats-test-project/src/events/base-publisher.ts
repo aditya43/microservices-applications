4ceab33293f0ff3d7cdad8a2db3fc432e31ef1f6
Aditya Hajare <aditya43@gmail.com>
2020-12-11 11:07:43 +0530

    Refactor

--- 12-Connecting-NATS-In-NodeJS/nats-test-project/src/events/base-publisher.ts
+++ 12-Connecting-NATS-In-NodeJS/nats-test-project/src/events/base-publisher.ts
@@ -1,4 +1,6 @@
+import { rejects } from 'assert';
 import { Stan } from 'node-nats-streaming';
+import { resolve } from 'path';
 import { Subjects } from './subjects';
 
 interface Event {
@@ -11,9 +13,15 @@ export abstract class Publisher<T extends Event> {
 
     constructor(private client: Stan) {}
 
-    publish(data: T['data']) {
-        this.client.publish(this.subject, JSON.stringify(data), () => {
-            console.log('Event Published!');
+    publish(data: T['data']): Promise<void> {
+        return new Promise((resolve, reject) => {
+            this.client.publish(this.subject, JSON.stringify(data), (err) => {
+                if (err) {
+                    return reject(err);
+                }
+
+                resolve();
+            });
         });
     }
 }
