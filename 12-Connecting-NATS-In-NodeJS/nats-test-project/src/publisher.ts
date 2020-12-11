bcf2b7575346b9986eaa91291679237626ff64cb
Aditya Hajare <aditya43@gmail.com>
2020-12-11 11:09:53 +0530

    Refactor

--- 12-Connecting-NATS-In-NodeJS/nats-test-project/src/publisher.ts
+++ 12-Connecting-NATS-In-NodeJS/nats-test-project/src/publisher.ts
@@ -6,14 +6,18 @@ const client = nats.connect('ticketing', randomBytes(4).toString('hex'), {
     url: 'http://localhost:4222',
 });
 
-client.on('connect', () => {
+client.on('connect', async () => {
     console.log('Publisher successfully connected to NATS');
 
     const publisher = new TicketCreatedPublisher(client);
 
-    publisher.publish({
-        id: '123',
-        title: 'Aditya Test Concert Ticket',
-        price: 20,
-    });
+    try {
+        await publisher.publish({
+            id: '123',
+            title: 'Aditya Test Concert Ticket',
+            price: 20,
+        });
+    } catch (err) {
+        console.error(err);
+    }
 });
